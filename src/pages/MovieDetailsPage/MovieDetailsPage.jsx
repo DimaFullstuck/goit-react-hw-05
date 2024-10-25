8006e0692f3ec05b916e36905d8b98e4255bca17
Dima <dima1999ostapenko@gmail.com>
2024-10-25 09:40:07 +0300

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,24 +1,24 @@
-import { useParams, NavLink, Outlet } from "react-router-dom";
-import { useState, useEffect, Suspense } from "react";
-import { FaArrowLeftLong } from "react-icons/fa6";
-import { useLocation } from "react-router-dom";
-import useFetchData from "../../hooks/useFetchData";
-import ApiService from "../../api/ApiService";
-import AppContainer from "../../components/AppContainer/AppContainer";
-import AppSection from "../../components/AppSection/AppSection";
-import AppSecTitle from "../../components/AppSecTitle/AppSecTitle";
-import InfinityLoader from "../../components/Infinity/Infinity";
-import MovieInfo from "../../components/MovieInfo/MovieInfo";
-import CustomLink from "../../components/CustomLink/CustomLink";
-import styles from "./MovieDetails.module.css";
+import { useParams, NavLink, Outlet } from 'react-router-dom';
+import { useState, useEffect, Suspense } from 'react';
+import { FaArrowLeftLong } from 'react-icons/fa6';
+import { useLocation } from 'react-router-dom';
+import useFetchData from '../../hooks/useFetchData';
+import ApiService from '../../api/ApiService';
+import AppContainer from '../../components/AppContainer/AppContainer';
+import AppSection from '../../components/AppSection/AppSection';
+import AppSecTitle from '../../components/AppSecTitle/AppSecTitle';
+import InfinityLoader from '../../components/Infinity/Infinity';
+import MovieInfo from '../../components/MovieInfo/MovieInfo';
+import CustomLink from '../../components/CustomLink/CustomLink';
+import styles from './MovieDetailsPage.module.css';
 
 const MovieDetailsPage = () => {
   const { id } = useParams();
   const [item, setItem] = useState({});
   const location = useLocation();
-  const backLinkHref = location.state?.from ?? "/movies";
+  const backLinkHref = location.state?.from ?? '/movies';
 
-  const [itemLoading, itemError, fetchItemData] = useFetchData(async (id) => {
+  const [itemLoading, itemError, fetchItemData] = useFetchData(async id => {
     const responce = await ApiService.getMovieDetailsById(id);
     setItem(responce);
   });
